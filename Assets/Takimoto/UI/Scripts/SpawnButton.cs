ae64897b6307d49c8b258aa2882567395a559bcd
0315-pc <mocochankawaii00110953@gmail.com>
2025-11-25 14:01:28 +0900

    エフェクトとボタンの改良
    とりあえずエフェクトを作った(回復・ユニット被攻撃・城被攻撃)
    ボタンを改良した

--- Assets/Takimoto/UI/Scripts/SpawnButton.cs
+++ Assets/Takimoto/UI/Scripts/SpawnButton.cs
@@ -1,18 +1,25 @@
 ﻿using System.Threading;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SpawnButton : MonoBehaviour
 {
     public UnitData Unit; //ScriptableObjectをインスペクターに設定する
-    public Button Button;
-    private Image _image;
+    public Image SpawnButtonPrefab;
 
-    private float _timer = 0.0f;
+    private Image _imageComp;
+    private Button _buttonComp;
+    private TextMeshProUGUI _textComp;
+    private Vector2 _textCompStartPosition;
+
+    private float _timer = 10.0f;
     private Player _player;
     private bool _bPushed = false;
 
     [SerializeField] ObjectPoolTest objectPoolTest;
+
+    [SerializeField] Vector3 spawnPosition;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -20,16 +27,39 @@ public class SpawnButton : MonoBehaviour
         //ボタンの画像をモンスターのアイコンに変える
     //    Button.image.sprite = Monster.MonsterIcon;
 
-        Image[] Images = Button.GetComponentsInChildren<Image>();
+        Image[] Images = SpawnButtonPrefab.GetComponentsInChildren<Image>();
         for(int i = 0; i < Images.Length; i++)
         {
             if (Images[i].gameObject.name == "CoolDown")
             {
-                _image = Images[i];
+                _imageComp = Images[i];    
                 break;
             }
         }
-        _image.fillAmount = 0;
+        _imageComp.fillAmount = 0;
+
+        Button[] Buttons = SpawnButtonPrefab.GetComponentsInChildren<Button>();
+        for(int i = 0; i < Buttons.Length; i++)
+        {
+            if (Buttons[i].gameObject.name == "Button")
+            {
+                _buttonComp = Buttons[i];
+                break;
+            }
+        }
+
+        TextMeshProUGUI[] Texts = SpawnButtonPrefab.GetComponentsInChildren<TextMeshProUGUI>();
+        for(int i = 0; i < Texts.Length; i++)
+        {
+            if (Texts[i].gameObject.name == "Text_Cost")
+            {
+                _textComp = Texts[i];
+                _textComp.gameObject.SetActive(false);
+                _textCompStartPosition = _textComp.rectTransform.anchoredPosition;
+                _textComp.SetText("-" + Unit.UnitCost);
+                break;
+            }
+        }
 
         _player = FindAnyObjectByType<Player>();
 
@@ -40,13 +70,25 @@ public class SpawnButton : MonoBehaviour
     void FixedUpdate()
     {
         if (_bPushed)
-        {
+        {            
             _timer += Time.fixedDeltaTime;
-            _image.fillAmount += Time.fixedDeltaTime / Unit.UnitCoolDown;
+            Debug.Log("_timer:" + _timer);
+
+            if (_timer >= 0.5f)
+            {
+                _textComp.gameObject.SetActive(false);
+            }
+            else
+            {
+                Debug.Log("Text_Cost is moving");
+                _textComp.rectTransform.anchoredPosition += new Vector2(0.0f, 50.0f) * Time.fixedDeltaTime;
+            }
+
+            _imageComp.fillAmount += Time.fixedDeltaTime / Unit.UnitCoolDown;
             if (_timer > Unit.UnitCoolDown) //HERE USE UNITDATA INSTEAD !!!!!
             {
-                Button.interactable = true;
-                _image.fillAmount = 0;
+                _buttonComp.interactable = true;
+                _imageComp.fillAmount = 0;
                 _bPushed = false;
             }
         }
@@ -55,11 +97,11 @@ public class SpawnButton : MonoBehaviour
             //Debug.Log($"_player:{_player}, Unit:{Unit}, cost:{Unit.UnitCost}");
             if (_player.Money <= Unit.UnitCost)
             {
-                Button.interactable = false;
+                _buttonComp.interactable = false;
             }
             else
             {
-                Button.interactable = true;
+                _buttonComp.interactable = true;
             }
         }        
     }
@@ -67,12 +109,14 @@ public class SpawnButton : MonoBehaviour
     public void OnButtonDown_Spawn()
     {
         //Monsterをスポーンさせる
-        Vector3 position = Vector3.zero;
-        objectPoolTest.GetObj(position, Unit); //[2025/11/20]　プリンス　: 「, Unit」を追加した -> 適切のデータをユニットに与える
+        objectPoolTest.GetObj(spawnPosition, Unit); //[2025/11/20]　プリンス　: 「, Unit」を追加した -> 適切のデータをユニットに与える
 
-        Button.interactable = false;
+        _buttonComp.interactable = false;
         _timer = 0.0f;
         _player.Money -= Unit.UnitCost;
+
+        _textComp.gameObject.SetActive(true);
+        _textComp.rectTransform.anchoredPosition = _textCompStartPosition;
 
         _bPushed = true;
     }
