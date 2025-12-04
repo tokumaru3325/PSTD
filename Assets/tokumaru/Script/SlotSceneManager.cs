173c48a7c0726752536cb53ade248a09218bdd6e
tokumaru3325 <furufuru3325@gmail.com>
2025-12-04 14:18:20 +0900

    スロット改修

---
+++ Assets/tokumaru/Script/SlotSceneManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public delegate void SlotResult(int i);
+public class SlotSceneManager : MonoBehaviour
+{
+    private static SlotResult ak = (int i) => { };
+    private static bool open = false;
+    private static int slotType = 0;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public static void ChangeScene()
+    {
+        if(open == false)
+        {
+            SceneManager.LoadScene("Slot", LoadSceneMode.Additive);
+            open = true;
+        }
+        else
+        {
+            SceneManager.UnloadSceneAsync("Slot");
+            open = false;
+        }
+    }
+
+    public static void SetFunc(SlotResult sr)
+    {
+        ak += sr;
+    }
+}
