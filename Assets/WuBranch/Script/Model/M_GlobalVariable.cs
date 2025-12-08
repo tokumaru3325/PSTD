676c47de8682155ba3ccbef5ad2b27cb3022f29b
gene <cg100111@gmail.com>
2025-12-08 12:50:54 +0900

    先のコミットで漏らしたファイル

--- Assets/WuBranch/Script/Model/M_GlobalVariable.cs
+++ Assets/WuBranch/Script/Model/M_GlobalVariable.cs
@@ -5,11 +5,25 @@ public class M_GlobalVariable
     public MultiRoleType MultiRole { get; private set; }
 
     /// <summary>
+    /// プレイヤーの名前
+    /// </summary>
+    public string MyName { get; private set; }
+
+    /// <summary>
     /// マルチの役割を設定
     /// </summary>
     /// <param name="role">モード</param>
     public void SetRole(MultiRoleType role)
     {
         MultiRole = role;
+    }
+
+    /// <summary>
+    /// プレイヤーの名前を設定
+    /// </summary>
+    /// <param name="name">名前</param>
+    public void SetMyName(string name)
+    {
+        MyName = name;
     }
 }
