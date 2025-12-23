23bb24b0b461aaccc38960aca376e7371db6f402
0315-pc <mocochankawaii00110953@gmail.com>
2025-12-23 14:52:23 +0900

    バフ関連のやつを作った。
    バフマネージャー・複数のScriptableObjectなど。

---
+++ Assets/Takimoto/Buff/BuffData.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "BuffData", menuName = "Scriptable Objects/BuffData")]
+public class BuffData : ScriptableObject
+{
+    public float AttackPower = 0;
+    public float AttackSpeed = 0;
+    public float AttackRange = 0;
+    public float MoveSpeed = 0;
+}
---
+++ Assets/Takimoto/Buff/BuffManager.cs
@@ -0,0 +1,60 @@
+﻿using Cysharp.Threading.Tasks;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using UnityEngine;
+
+public enum BuffType
+{
+    None,
+    AttackPower,
+    AttackSpeed,
+    AttackRange,
+    MoveSpeed
+}
+
+public class BuffManager : MonoBehaviour
+{
+    [SerializeField]
+    private BuffData _buffData;
+
+    [SerializeField]
+    private BuffAttackPowerData _attackPowerData;
+    [SerializeField]
+    private BuffAttackSpeedData _attackSpeedData;
+
+    //private List<BuffType> _currentBuff = new List<BuffType>();
+
+    public async UniTask AddBuff(BuffType buff)
+    {
+        BuffTypeData addBuff = null;
+        switch (buff)
+        {
+        case BuffType.AttackPower:
+                addBuff = _attackPowerData;               
+                break;
+
+        case BuffType.AttackSpeed:
+                addBuff = _attackSpeedData;
+                break;
+
+        case BuffType.AttackRange:
+                break;
+
+        case BuffType.MoveSpeed:
+                break;
+        }
+
+        if (addBuff)
+        {
+            _buffData.AttackPower += addBuff.BuffValue;
+            await UniTask.Delay(addBuff.BuffTime);
+            _buffData.AttackPower -= addBuff.BuffValue;
+        }        
+    }
+
+    public void TestButtonDown()
+    {
+        AddBuff(BuffType.AttackPower);
+    }
+}
