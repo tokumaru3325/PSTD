1e84e94ae1acf20782260432ce0af1e9273b5c8c
tokumaru3325 <furufuru3325@gmail.com>
2025-12-23 14:13:36 +0900

    ん

--- Assets/Takimoto/Player.cs
+++ Assets/Takimoto/Player.cs
@@ -9,9 +9,6 @@ public class Player : MonoBehaviour
     void Start()
     {
         Money = 0.0f;
-        //得丸陽生　2025/12/16 start
-        SlotSceneManager.AddFuncToMoneySlot(UpdateStateBySlotResult);
-        //得丸陽生 end
     }
 
     // Update is called once per frame
@@ -19,12 +16,4 @@ public class Player : MonoBehaviour
     {
         Money += Time.deltaTime * 3;
     }
-
-    //得丸陽生　2025/12/16 start
-    void UpdateStateBySlotResult(int i)
-    {
-        //ここに効果を書く
-        Money += i * 100;
-    }
-    //得丸陽生 end
 }
