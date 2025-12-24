aa618dba879e77d4633c3524013d16e32f206ded
tokumaru3325 <furufuru3325@gmail.com>
2025-12-24 11:46:05 +0900

    β最終調整

--- Assets/Prinz/Scripts/State_Interfaces/AttackState.cs
+++ Assets/Prinz/Scripts/State_Interfaces/AttackState.cs
@@ -1,6 +1,6 @@
-using System;
+﻿using System;
 using System.Threading;
-using UnityEditor.Experimental.GraphView;
+//using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class AttackState : IUnitState
--- Assets/tokumaru/Script/SlotSceneManager.cs
+++ Assets/tokumaru/Script/SlotSceneManager.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using UnityEditor.SceneManagement;
+//using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
