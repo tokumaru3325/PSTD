d13616966a2a5e3c54581b89aad307b65c5f01ef
gene <cg100111@gmail.com>
2026-01-02 14:36:45 +0900

    城のタイプ列挙型を追加 ルームにいるプレイヤの準備状態にnull(プレイヤがいない)を追加

---
+++ Assets/WuBranch/Script/Common/CastleType.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+public enum CastleType
+{
+    Null = 0,       // ない、プレイヤーがいない
+    Castle1,        // 城1
+    Castle2,        // 城2
+    Castle3         // 城3
+}
--- Assets/WuBranch/Script/Common/GameReadyState.cs
+++ Assets/WuBranch/Script/Common/GameReadyState.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 /// </summary>
 public enum GameReadyState
 {
+    Null,       // ない、プレイヤーがいない
     Preparing,  // 準備中
     Ready       // 準備完了
 }
