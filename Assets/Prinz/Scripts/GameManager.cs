f09c589c3997c60efa80e81486961e07033ee1b6
Griselune <philoch74@gmail.com>
2026-01-14 10:17:03 +0900

    GameManagerとDebugManagerの実装 ゲーム終了中の処理を追加

--- Assets/Prinz/Scripts/GameManager.cs
+++ Assets/Prinz/Scripts/GameManager.cs
@@ -1,8 +1,15 @@
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public bool IsGameFinished {  get; private set; }
+    public static bool IsGameFinished {  get; private set; }
+    public int unitCount {  get; private set; } //serialNumber
+
+    Coroutine Ending;
+
+    public static event Action<bool, string> GameEnding;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -17,10 +24,30 @@ public class GameManager : MonoBehaviour
         
     }
 
+    private void OnDisable()
+    {
+    //    Time.timeScale = 1.0f;
+        M_Tower.OnPlayerDeath -= OnPlayerDeathNotify;
+    }
+
     private void OnPlayerDeathNotify(string deadplayertag)
     {
-
-
         IsGameFinished = true;
+     //   Time.timeScale = 0;
+        GameEnding?.Invoke(IsGameFinished, deadplayertag);
+        Ending = StartCoroutine(EndingScene());
+    }
+
+    public int OnUnitSpawn()
+    {
+        unitCount++;
+        return unitCount;
+    }
+
+    private IEnumerator EndingScene()
+    {
+        //do something like wait before loading ending scene and stuff
+
+        yield break;
     }
 }
