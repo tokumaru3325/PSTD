dd559c5148911bc2b106511870b285c9e2116fdd
0315-pc <mocochankawaii00110953@gmail.com>
2026-01-20 13:27:20 +0900

    矢を飛ばす処理を書いてる途中

--- Assets/Prinz/Units/Archer/Arrow.cs
+++ Assets/Prinz/Units/Archer/Arrow.cs
@@ -1,10 +1,9 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Arrow : MonoBehaviour
 {
     [SerializeField] private float _speed;
     private Vector3 _target;
-    private Vector3 _position;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,13 +18,12 @@ public class Arrow : MonoBehaviour
 
     public void Initialize(Vector3 position, Vector3 enemyposition)
     {
-        _position = position;
         _target = enemyposition;
     }
 
     private void Move()
     {
         float step = _speed * Time.deltaTime;
-        transform.position = Vector3.MoveTowards(_position, _target, step);
+        transform.position = Vector3.MoveTowards(transform.position, _target, step);
     }
 }
